8b328b244fd8eccacb7f75c9e11fcb493b1e6c57
DBC Apprentice <apprentice@devbootcamp.com>
2012-09-22 09:30:39 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MouseTrap::Application.config.secret_token = 'd469da8402a9912b5837243f7bc8e2cedddc57cbb6d8d2ef7b88512d62a67b9208c5976ccf87740b5a8f0671da7f991f23e35aeb2ea7416e605614af7017e88d'
